6e58607b4dd9a35429c9c4c1a4400e03f897e77d
AdletMier <eypk777@gmail.com>
2023-03-05 23:24:12 +0900

    Add Main Channel for boxing types in server/main.rs

--- furuyoni_server/src/main_channels.rs
+++ furuyoni_server/src/main_channels.rs
@@ -1,16 +1,18 @@
 extern crate furuyoni_lib;
 
+use std::error::Error;
+
 use furuyoni_lib::net::{frames::{
     LobbyToPlayerRequestData, LobbyResponse,
     GameNotification, GameToPlayerRequestData, PlayerResponse, GameToPlayerResponseFrame, LobbyNotification, PlayerToGameRequestFrame,
-}, Requester, message_sender::MessageSender, Responder};
+}, Requester, message_sender::MessageSender, Responder, connection::ParseError};
 
-struct MainChannels{
-    lobby_to_player_requester : Box<dyn Requester<LobbyToPlayerRequestData, Response = LobbyResponse>>,
+pub struct MainChannels{
+    lobby_to_player_requester : Box<dyn Requester<LobbyToPlayerRequestData, Response = LobbyResponse, Error = ParseError>>,
     lobby_to_player_notifier : Box<dyn MessageSender<LobbyNotification>>,
-    lobby_to_player_responder : Box<dyn Responder<GameToPlayerResponseFrame, Request = PlayerToGameRequestFrame>>,
-    game_to_player_requester : Box<dyn Requester<GameToPlayerRequestData, Response = PlayerResponse>>,
+    lobby_to_player_responder : Box<dyn Responder<GameToPlayerResponseFrame, Request = PlayerToGameRequestFrame, Error = ParseError>>,
+    game_to_player_requester : Box<dyn Requester<GameToPlayerRequestData, Response = PlayerResponse, Error = ParseError>>,
     game_to_player_notifier : Box<dyn MessageSender<GameNotification>>,
-    game_to_player_responder : Box<dyn Responder<GameToPlayerResponseFrame, Request = PlayerToGameRequestFrame>>
+    game_to_player_responder : Box<dyn Responder<GameToPlayerResponseFrame, Request = PlayerToGameRequestFrame, Error = ParseError>>
 }
 
